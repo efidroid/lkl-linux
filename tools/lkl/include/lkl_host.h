55cd30850759075b504f7182b6d8dd108fc25b46
Michael Zimmermann <sigmaepsilon92@gmail.com>
2021-04-09 18:10:25 +0200

    HACK: remove duplicate `struct iovec`

--- tools/lkl/include/lkl_host.h
+++ tools/lkl/include/lkl_host.h
@@ -19,14 +19,7 @@ int lkl_printf(const char *fmt, ...);
 
 extern char lkl_virtio_devs[4096];
 
-#ifdef CONFIG_AUTO_LKL_POSIX_HOST
 #include <sys/uio.h>
-#else
-struct iovec {
-	void *iov_base;
-	unsigned long iov_len;
-};
-#endif
 
 extern struct lkl_dev_blk_ops lkl_dev_blk_ops;
 
